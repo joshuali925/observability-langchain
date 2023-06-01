df53f2a75af9d09bc3b29c5f491146ed43f4f41a
Joshua Li <joshuali925@gmail.com>
2023-06-01 21:46:01 +0000

    add cat indices and index selector
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- public/components/event_analytics/explorer/llm/input.tsx
+++ public/components/event_analytics/explorer/llm/input.tsx
@@ -7,6 +7,8 @@ import {
   EuiButton,
   EuiButtonIcon,
   EuiCode,
+  EuiComboBox,
+  EuiComboBoxOptionOption,
   EuiCopy,
   EuiFieldText,
   EuiFlexGroup,
@@ -14,35 +16,35 @@ import {
   EuiSpacer,
   EuiText,
 } from '@elastic/eui';
-import React, { useEffect, useRef, useState } from 'react';
+import { CatIndicesResponse } from '@opensearch-project/opensearch/api/types';
+import React, { Reducer, useEffect, useReducer, useRef, useState } from 'react';
 import { LANGCHAIN_API } from '../../../../../common/constants/llm';
+import { DSL_BASE, DSL_CAT } from '../../../../../common/constants/shared';
 import { getOSDHttp } from '../../../../../common/utils';
 
 interface Props {
   handleQueryChange: (query: string) => void;
 }
-
 export const LLMInput: React.FC<Props> = (props) => {
-  const indexRef = useRef<HTMLInputElement>(null);
-  const timeFieldRef = useRef<HTMLInputElement>(null);
   const questionRef = useRef<HTMLInputElement>(null);
+  const { data, loading } = useCatIndices();
   const [query, setQuery] = useState('');
+  const [selectedIndex, setSelectedIndex] = useState<
+    Array<EuiComboBoxOptionOption<{ label: string }>>
+  >([{ label: 'opensearch_dashboards_sample_data_flights' }]);
 
   useEffect(() => {
-    if (indexRef.current) {
-      indexRef.current.value = 'opensearch_dashboards_sample_data_flights';
-    }
     if (questionRef.current) {
-      questionRef.current.value = 'How many flights were there in the past hour';
       questionRef.current.value = 'what are the longest flights in the past day';
     }
   }, []);
 
   const request = async () => {
+    if (!selectedIndex.length) return;
     const response = await getOSDHttp().post(LANGCHAIN_API.PPL_GENERATOR, {
       body: JSON.stringify({
         question: questionRef.current?.value,
-        index: indexRef.current?.value,
+        index: selectedIndex[0].label,
         timeField: '',
       }),
     });
@@ -55,7 +57,17 @@ export const LLMInput: React.FC<Props> = (props) => {
     <>
       <EuiFlexGroup>
         <EuiFlexItem grow={1}>
-          <EuiFieldText prepend={['Index']} fullWidth inputRef={indexRef} />
+          <EuiComboBox
+            placeholder="Select an index"
+            fullWidth
+            isClearable={false}
+            prepend={['Index']}
+            singleSelection={{ asPlainText: true }}
+            isLoading={loading}
+            options={data}
+            selectedOptions={selectedIndex}
+            onChange={(index) => setSelectedIndex(index)}
+          />
         </EuiFlexItem>
         <EuiFlexItem grow={2}>
           <EuiFieldText prepend={['Question']} fullWidth inputRef={questionRef} />
@@ -85,4 +97,47 @@ export const LLMInput: React.FC<Props> = (props) => {
       <EuiSpacer />
     </>
   );
+};
+
+interface State<T> {
+  data?: T;
+  loading: boolean;
+  error?: Error;
+}
+type Action<T> =
+  | { type: 'request' }
+  | { type: 'success'; payload: State<T>['data'] }
+  | { type: 'failure'; error: Required<State<T>['error']> };
+type GenericReducer<T = any> = Reducer<State<T>, Action<T>>;
+const genericReducer: GenericReducer = (state, action) => {
+  switch (action.type) {
+    case 'request':
+      return { loading: true };
+    case 'success':
+      return { loading: false, data: action.payload };
+    case 'failure':
+      return { loading: false, error: action.error };
+    default:
+      return state;
+  }
+};
+export const useCatIndices = () => {
+  const reducer: GenericReducer<Array<{ label: string }>> = genericReducer;
+  const [state, dispatch] = useReducer(reducer, { loading: false });
+  const [refresh, setRefresh] = useState({});
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    dispatch({ type: 'request' });
+    getOSDHttp()
+      .get(`${DSL_BASE}${DSL_CAT}`, { query: { format: 'json' }, signal: abortController.signal })
+      .then((payload: CatIndicesResponse) =>
+        dispatch({ type: 'success', payload: payload.map((meta) => ({ label: meta.index! })) })
+      )
+      .catch((error) => dispatch({ type: 'failure', error }));
+
+    return () => abortController.abort();
+  }, [refresh]);
+
+  return { ...state, refresh: () => setRefresh({}) };
 };
