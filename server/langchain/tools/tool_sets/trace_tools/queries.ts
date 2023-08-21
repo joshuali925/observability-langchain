f9734f9d16d8cde7a3da103cc8949a92098b9ec9
Daniel Dong <danieldong51@amazon.com>
2023-08-21 14:35:52 -0700

    Added tools for getting traces and services
    Signed-off-by: Daniel Dong <danieldong51@amazon.com>

--- server/langchain/tools/tool_sets/trace_tools/queries.ts
+++ server/langchain/tools/tool_sets/trace_tools/queries.ts
@@ -3,10 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { DATA_PREPPER_INDEX_NAME } from 'common/constants/trace_analytics';
 import { TraceAnalyticsMode } from 'public/components/trace_analytics/home';
+import { ServiceObject } from 'public/components/trace_analytics/components/common/plots/service_map';
+import {
+  SERVICE_MAP_MAX_NODES,
+  TRACES_MAX_NUM,
+} from '../../../../../common/constants/trace_analytics';
 
-import { PluginToolsFactory } from '../../tools_factory/tools_factory';
 import { OpenSearchClient } from '../../../../../../../src/core/server';
 
 export async function getIndexName(opensearchClient: OpenSearchClient) {
@@ -50,7 +53,7 @@ export const getDashboardQuery = (indexName: string) => {
           field: 'traceGroup',
           size: 10000,
         },
-        index: indexName,
+        // index: indexName,
         aggs: {
           average_latency: {
             scripted_metric: {
@@ -122,4 +125,486 @@ export const getDashboardQuery = (indexName: string) => {
       },
     },
   };
+};
+
+export const getTracesQuery = (mode: TraceAnalyticsMode, traceId: string = '') => {
+  const jaegerQuery = {
+    size: 0,
+    query: {
+      bool: {
+        must: [],
+        filter: [],
+        should: [],
+        must_not: [],
+      },
+    },
+    aggs: {
+      traces: {
+        terms: {
+          field: 'traceID',
+          size: TRACES_MAX_NUM,
+        },
+        aggs: {
+          latency: {
+            max: {
+              script: {
+                source: `
+                if (doc.containsKey('duration') && !doc['duration'].empty) {
+                  return Math.round(doc['duration'].value) / 1000.0
+                }
+
+                return 0
+                `,
+                lang: 'painless',
+              },
+            },
+          },
+          trace_group: {
+            terms: {
+              field: 'traceGroup',
+              size: 1,
+            },
+          },
+          error_count: {
+            filter: {
+              term: {
+                'tag.error': true,
+              },
+            },
+          },
+          last_updated: {
+            max: {
+              script: {
+                source: `
+                if (doc.containsKey('startTime') && !doc['startTime'].empty && doc.containsKey('duration') && !doc['duration'].empty) {
+                  return (Math.round(doc['duration'].value) + Math.round(doc['startTime'].value)) / 1000.0
+                }
+
+                return 0
+                `,
+                lang: 'painless',
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+  const dataPrepperQuery = {
+    size: 0,
+    query: {
+      bool: {
+        must: [],
+        filter: [],
+        should: [],
+        must_not: [],
+      },
+    },
+    aggs: {
+      traces: {
+        terms: {
+          field: 'traceId',
+          size: TRACES_MAX_NUM,
+        },
+        aggs: {
+          latency: {
+            max: {
+              script: {
+                source: `
+                if (doc.containsKey('traceGroupFields.durationInNanos') && !doc['traceGroupFields.durationInNanos'].empty) {
+                  return Math.round(doc['traceGroupFields.durationInNanos'].value / 10000) / 100.0
+                }
+                return 0
+                `,
+                lang: 'painless',
+              },
+            },
+          },
+          trace_group: {
+            terms: {
+              field: 'traceGroup',
+              size: 1,
+            },
+          },
+          error_count: {
+            filter: {
+              term: {
+                'traceGroupFields.statusCode': '2',
+              },
+            },
+          },
+          last_updated: {
+            max: {
+              field: 'traceGroupFields.endTime',
+            },
+          },
+        },
+      },
+    },
+  };
+  // if (traceId) {
+  //   jaegerQuery.query.bool.must.push({
+  //     term: {
+  //       "traceID": traceId,
+  //     },
+  //   });
+  //   dataPrepperQuery.query.bool.must.push({
+  //     term: {
+  //       traceId,
+  //     },
+  //   });
+  // }
+  return mode === 'jaeger' ? jaegerQuery : dataPrepperQuery;
+};
+
+export const getServicesQuery = (
+  serviceNames: string[],
+  map: ServiceObject,
+  mode: TraceAnalyticsMode
+) => {
+  // const traceGroupFilter = new Set(
+  //     DSL?.query?.bool.must
+  //       .filter((must: any) => must.term?.['traceGroup'])
+  //       .map((must: any) => must.term.traceGroup) || []
+  //   );
+
+  // const targetResource =
+  // traceGroupFilter.size > 0
+  //     ? [].concat(
+  //         ...[].concat(
+  //         ...serviceNames.map((service) =>
+  //             map[service].traceGroups
+  //             .filter((traceGroup) => traceGroupFilter.has(traceGroup.traceGroup))
+  //             .map((traceGroup) => traceGroup.targetResource)
+  //         )
+  //         )
+  //     )
+  //     : [].concat(...Object.keys(map).map((service) => getServiceMapTargetResources(map, service)));
+  const jaegerQuery = {
+    size: 0,
+    query: {
+      bool: {
+        must: [],
+        should: [],
+        must_not: [],
+        filter: [
+          {
+            terms: {
+              'process.serviceName': serviceNames,
+            },
+          },
+          {
+            bool: {
+              should: [
+                {
+                  bool: {
+                    filter: [
+                      {
+                        bool: {
+                          must_not: {
+                            term: {
+                              references: {
+                                value: [],
+                              },
+                            },
+                          },
+                        },
+                      },
+                    ],
+                  },
+                },
+                {
+                  bool: {
+                    must: {
+                      term: {
+                        references: {
+                          value: [],
+                        },
+                      },
+                    },
+                  },
+                },
+              ],
+              adjust_pure_negative: true,
+              boost: 1,
+            },
+          },
+        ],
+      },
+    },
+    aggregations: {
+      service_name: {
+        terms: {
+          field: 'process.serviceName',
+          size: SERVICE_MAP_MAX_NODES,
+          min_doc_count: 1,
+          shard_min_doc_count: 0,
+          show_term_doc_count_error: false,
+          order: [
+            {
+              _count: 'desc',
+            },
+            {
+              _key: 'asc',
+            },
+          ],
+        },
+        aggregations: {
+          average_latency_nanos: {
+            avg: {
+              field: 'duration',
+            },
+          },
+          average_latency: {
+            bucket_script: {
+              buckets_path: {
+                count: '_count',
+                latency: 'average_latency_nanos.value',
+              },
+              script: 'Math.round(params.latency / 10) / 100.0',
+            },
+          },
+          error_count: {
+            filter: {
+              term: {
+                'tag.error': true,
+              },
+            },
+          },
+          error_rate: {
+            bucket_script: {
+              buckets_path: {
+                total: '_count',
+                errors: 'error_count._count',
+              },
+              script: 'params.errors / params.total * 100',
+            },
+          },
+        },
+      },
+    },
+  };
+
+  const dataPrepperQuery = {
+    size: 0,
+    query: {
+      bool: {
+        must: [],
+        should: [],
+        must_not: [],
+        filter: [
+          {
+            terms: {
+              serviceName: serviceNames,
+            },
+          },
+          {
+            bool: {
+              should: [
+                {
+                  bool: {
+                    filter: [
+                      {
+                        bool: {
+                          must_not: {
+                            term: {
+                              parentSpanId: {
+                                value: '',
+                              },
+                            },
+                          },
+                        },
+                      },
+                      {
+                        terms: {
+                          // name: targetResource,
+                        },
+                      },
+                    ],
+                  },
+                },
+                {
+                  bool: {
+                    must: {
+                      term: {
+                        parentSpanId: {
+                          value: '',
+                        },
+                      },
+                    },
+                  },
+                },
+              ],
+              adjust_pure_negative: true,
+              boost: 1,
+            },
+          },
+        ],
+      },
+    },
+    aggregations: {
+      service_name: {
+        terms: {
+          field: 'serviceName',
+          size: SERVICE_MAP_MAX_NODES,
+          min_doc_count: 1,
+          shard_min_doc_count: 0,
+          show_term_doc_count_error: false,
+          order: [
+            {
+              _count: 'desc',
+            },
+            {
+              _key: 'asc',
+            },
+          ],
+        },
+        aggregations: {
+          average_latency_nanos: {
+            avg: {
+              field: 'durationInNanos',
+            },
+          },
+          average_latency: {
+            bucket_script: {
+              buckets_path: {
+                count: '_count',
+                latency: 'average_latency_nanos.value',
+              },
+              script: 'Math.round(params.latency / 10000) / 100.0',
+            },
+          },
+          error_count: {
+            filter: {
+              term: {
+                'status.code': '2',
+              },
+            },
+          },
+          error_rate: {
+            bucket_script: {
+              buckets_path: {
+                total: '_count',
+                errors: 'error_count._count',
+              },
+              script: 'params.errors / params.total * 100',
+            },
+          },
+        },
+      },
+    },
+  };
+  // if (DSL.custom?.timeFilter.length > 0) {
+  // jaegerQuery.query.bool.must.push(...DSL.custom.timeFilter);
+  // dataPrepperQuery.query.bool.must.push(...DSL.custom.timeFilter);
+  // }
+  return mode === 'jaeger' ? jaegerQuery : dataPrepperQuery;
+  // const query = {
+  //     size: 0,
+  //     query: {
+  //       bool: {
+  //         must: [],
+  //         filter: [],
+  //         should: [],
+  //         must_not: [],
+  //       },
+  //     },
+  //     aggregations: {
+  //         service_name: {
+  //           terms: {
+  //             field: 'serviceName',
+  //             size: SERVICE_MAP_MAX_NODES,
+  //             min_doc_count: 1,
+  //             shard_min_doc_count: 0,
+  //             show_term_doc_count_error: false,
+  //             order: [
+  //               {
+  //                 _count: 'desc',
+  //               },
+  //               {
+  //                 _key: 'asc',
+  //               },
+  //             ],
+  //           },
+  //           aggregations: {
+  //             average_latency_nanos: {
+  //               avg: {
+  //                 field: 'durationInNanos',
+  //               },
+  //             },
+  //             average_latency: {
+  //               bucket_script: {
+  //                 buckets_path: {
+  //                   count: '_count',
+  //                   latency: 'average_latency_nanos.value',
+  //                 },
+  //                 script: 'Math.round(params.latency / 10000) / 100.0',
+  //               },
+  //             },
+  //             error_count: {
+  //               filter: {
+  //                 term: {
+  //                   'status.code': '2',
+  //                 },
+  //               },
+  //             },
+  //             error_rate: {
+  //               bucket_script: {
+  //                 buckets_path: {
+  //                   total: '_count',
+  //                   errors: 'error_count._count',
+  //                 },
+  //                 script: 'params.errors / params.total * 100',
+  //               },
+  //             },
+  //           },
+  //         },
+  //       },
+  //   };
+  // //   if (mode === 'jaeger') {
+  // //     if (serviceName) {
+  // //       query.query.bool.must.push({
+  // //         term: {
+  // //           "process.serviceName": serviceName,
+  // //         },
+  // //       });
+  // //     }
+  // //     DSL?.custom?.serviceNames?.map((service: string) => {
+  // //       query.query.bool.must.push({
+  // //         term: {
+  // //           "process.serviceName": service,
+  // //         },
+  // //       });
+  // //     });
+  // //     DSL?.custom?.serviceNamesExclude?.map((service: string) => {
+  // //       query.query.bool.must_not.push({
+  // //         term: {
+  // //           "process.serviceName": service,
+  // //         },
+  // //       });
+  // //     });
+  // //   } else {
+  // //     if (serviceName) {
+  // //       query.query.bool.must.push({
+  // //         term: {
+  // //           "serviceName": serviceName,
+  // //         },
+  // //       });
+  // //     }
+  // //     DSL?.custom?.serviceNames?.map((service: string) => {
+  // //       query.query.bool.must.push({
+  // //         term: {
+  // //           "serviceName": service,
+  // //         },
+  // //       });
+  // //     });
+  // //     DSL?.custom?.serviceNamesExclude?.map((service: string) => {
+  // //       query.query.bool.must_not.push({
+  // //         term: {
+  // //           "serviceName": service,
+  // //         },
+  // //       });
+  // //     });
+  // //   }
+  //   return query;
 };
--- server/langchain/tools/tool_sets/traces.ts
+++ server/langchain/tools/tool_sets/traces.ts
@@ -4,30 +4,48 @@
  */
 
 import { DynamicTool } from 'langchain/tools';
+import {
+  DATA_PREPPER_SERVICE_INDEX_NAME,
+  JAEGER_SERVICE_INDEX_NAME,
+} from 'common/constants/trace_analytics';
+import { None } from 'vega';
 import { PluginToolsFactory } from '../tools_factory/tools_factory';
 
 import { swallowErrors } from '../../utils/utils';
-import { getDashboardQuery, getIndexName } from './trace_tools/queries';
+import {
+  getDashboardQuery,
+  getIndexName,
+  getServicesQuery,
+  getTracesQuery,
+} from './trace_tools/queries';
+import { ServiceObject } from '../../../../public/components/trace_analytics/components/common/plots/service_map';
 
 export class TracesTools extends PluginToolsFactory {
   static TOOL_NAMES = {
     TRACE_GROUPS: 'Get trace groups',
     SERVICES: 'Get trace services',
+    TRACES: 'Get traces',
   } as const;
 
   toolsList = [
     new DynamicTool({
       name: TracesTools.TOOL_NAMES.TRACE_GROUPS,
       description:
-        'Use this to get information about each trace group. The key is the name of the trace group, the doc_count is the number of spans, the trace_count is the number of traces, the average latency is measured in milliseconds, and the error rate is the percentage of traces with errors. You can specify a start and end time, formatted in %',
+        'Use this to get information about each trace group. The key is the name of the trace group, the doc_count is the number of spans, the trace_count is the number of traces, the average latency is measured in milliseconds, and the error rate is the percentage of traces with errors. This tool takes in no inputs.',
       func: swallowErrors(async () => this.getTraceGroupsQuery()),
     }),
-    // new DynamicTool({
-    //   name: TracesTools.TOOL_NAMES.SERVICES,
-    //   description:
-    //     'Use this to get information about each service in the system of traces. The key is the name of the service, the average latency is measured in milliseconds, the error rate is the percentage of spans in that service with errors, the throughput is the number of traces through that service, and the trace_count is the number of traces',
-    //   func: swallowErrors(async () => this.getServicesQuery().then()),
-    // }),
+    new DynamicTool({
+      name: TracesTools.TOOL_NAMES.SERVICES,
+      description:
+        'Use this to get information about each service. The key is the name of the service, the average latency is measured in milliseconds, the error rate is the percentage of spans in that service with errors, the throughput is the number of traces through that service, and the trace_count is the number of traces. This tool takes in no inputs.',
+      func: swallowErrors(async () => this.getServicesQuery().then()),
+    }),
+    new DynamicTool({
+      name: TracesTools.TOOL_NAMES.TRACES,
+      description:
+        'Use this to get information about each trace. The key is the ID of the trace. The doc_count is the number of spans in that particular trace. The latency is measured in ms. If the error count is not 0, that trace had an error. The trace group is what trace group the trace belongs to. This tool takes in no inputs.',
+      func: swallowErrors(async () => this.getTracesQuery().then()),
+    }),
   ];
 
   public async getTraceGroupsQuery() {
@@ -35,7 +53,7 @@ export class TracesTools extends PluginToolsFactory {
     const query = getDashboardQuery(await indexName);
     try {
       const traceGroupsResponse = await this.observabilityClient.callAsCurrentUser('search', {
-        body: query,
+        body: JSON.stringify(query),
       });
       console.log(traceGroupsResponse.aggregations.trace_group_name.buckets);
       return JSON.stringify(traceGroupsResponse.aggregations.trace_group_name.buckets);
@@ -44,5 +62,46 @@ export class TracesTools extends PluginToolsFactory {
     }
   }
 
-  public async getServicesQuery() {}
+  public async getServicesQuery() {
+    const indexName = getIndexName(this.opensearchClient);
+    const DSL = {
+      query: {
+        bool: {
+          must: [],
+          filter: [],
+          should: [],
+          must_not: [],
+        },
+      },
+    };
+    const map: ServiceObject = {};
+    const query = getServicesQuery([], map, 'data_prepper');
+    console.log(JSON.stringify(query));
+    try {
+      const servicesResponse = await this.observabilityClient.callAsCurrentUser('search', {
+        body: JSON.stringify(query),
+      });
+      console.log(servicesResponse.aggregations.service_name.buckets);
+      return JSON.stringify(servicesResponse.aggregations.service_name.buckets);
+    } catch (error) {
+      return 'error in running trace groups query' + error;
+    }
+  }
+
+  public async getTracesQuery() {
+    const indexName = getIndexName(this.opensearchClient);
+
+    const query = getTracesQuery('data_prepper', '');
+    console.log(JSON.stringify(query));
+    try {
+      const tracesResponse = await this.observabilityClient.callAsCurrentUser('search', {
+        body: JSON.stringify(query),
+      });
+      console.log(tracesResponse);
+      console.log(tracesResponse.aggregations.traces.buckets);
+      return JSON.stringify(tracesResponse.aggregations.traces.buckets);
+    } catch (error) {
+      return 'error in running trace groups query' + error;
+    }
+  }
 }
