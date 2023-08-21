3ef912f8706ad3e8a4373691528eb660a3b1a93c
Daniel Dong <danieldong51@amazon.com>
2023-08-21 16:54:43 -0700

    Cleaned up comments + removed broken services query
    Signed-off-by: Daniel Dong <danieldong51@amazon.com>

--- server/langchain/tools/tool_sets/traces.ts
+++ server/langchain/tools/tool_sets/traces.ts
@@ -35,12 +35,6 @@ export class TracesTools extends PluginToolsFactory {
       func: swallowErrors(async () => this.getTraceGroupsQuery()),
     }),
     new DynamicTool({
-      name: TracesTools.TOOL_NAMES.SERVICES,
-      description:
-        'Use this to get information about each service. The key is the name of the service, the average latency is measured in milliseconds, the error rate is the percentage of spans in that service with errors, the throughput is the number of traces through that service, and the trace_count is the number of traces. This tool takes in no inputs.',
-      func: swallowErrors(async () => this.getServicesQuery().then()),
-    }),
-    new DynamicTool({
       name: TracesTools.TOOL_NAMES.TRACES,
       description:
         'Use this to get information about each trace. The key is the ID of the trace. The doc_count is the number of spans in that particular trace. The latency is measured in ms. If the error count is not 0, that trace had an error. The trace group is what trace group the trace belongs to. This tool takes in no inputs.',
@@ -57,32 +51,6 @@ export class TracesTools extends PluginToolsFactory {
       });
       console.log(traceGroupsResponse.aggregations.trace_group_name.buckets);
       return JSON.stringify(traceGroupsResponse.aggregations.trace_group_name.buckets);
-    } catch (error) {
-      return 'error in running trace groups query' + error;
-    }
-  }
-
-  public async getServicesQuery() {
-    const indexName = getIndexName(this.opensearchClient);
-    const DSL = {
-      query: {
-        bool: {
-          must: [],
-          filter: [],
-          should: [],
-          must_not: [],
-        },
-      },
-    };
-    const map: ServiceObject = {};
-    const query = getServicesQuery([], map, 'data_prepper');
-    console.log(JSON.stringify(query));
-    try {
-      const servicesResponse = await this.observabilityClient.callAsCurrentUser('search', {
-        body: JSON.stringify(query),
-      });
-      console.log(servicesResponse.aggregations.service_name.buckets);
-      return JSON.stringify(servicesResponse.aggregations.service_name.buckets);
     } catch (error) {
       return 'error in running trace groups query' + error;
     }
