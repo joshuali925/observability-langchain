eb4538f9d943f996d61be494c8b569f0aff07987
Daniel Dong <danieldong51@amazon.com>
2023-08-28 15:03:38 -0700

    Fixed typos and typing errors
    Signed-off-by: Daniel Dong <danieldong51@amazon.com>

--- server/langchain/chains/filter_generator.ts
+++ server/langchain/chains/filter_generator.ts
@@ -18,7 +18,7 @@ The end time is the end of the time period the user is asking about. For example
 
 Time formats can be absolute or relative. 
 
-If absolute, they are structured as "%year-%month-%dateT%hour:$minute.%second". An example would be "2004-08-01T16:03:02" meaning a time of August 1st, 2004, 4:03.022 PM, in the Pacific Time Zone. Another example would be the user specifying "2018", so your generation would be "2018-01-01T00:00:00".
+If absolute, they are structured as "%year-%month-%dateT%hour:%minute.%second". An example would be "2004-08-01T16:03:02" meaning a time of August 1st, 2004, 4:03.022 PM, in the Pacific Time Zone. Another example would be the user specifying "2018", so your generation would be "2018-01-01T00:00:00".
 
 If relative, they are relative to "now", and are structured as "now+%n%u" for in the future, or "now-%n%u" for in the past. The %n is a number, and %u is a unit of measurement. For units, "s" means seconds, "m" means minutes, "h" means hours, "d" means days, "w" means weeks, "M" means months, and "y" means years. An example would be "now-3w" meaning 3 weeks before now, and "now+4y" meaning 4 years from now. 
 
@@ -26,7 +26,7 @@ If relative, they are relative to "now", and are structured as "now+%n%u" for in
 
 Use the following steps to generate the start and end times:
 
-Step 1. Use the user's query to write the start time in either absolute or relative time format. If you cannot generate one, set it to the year 2000.
+Step 1. Use the user's query to write the start time in either absolute or relative time format. If you cannot generate one, set it to 'now-15m'.
 
 Step 2. Use the user's query to write the end time in either absolute or relative time format. If you cannot generate one, set it to now.
 
--- server/langchain/tools/tool_sets/trace_tools/filters.ts
+++ server/langchain/tools/tool_sets/trace_tools/filters.ts
@@ -5,8 +5,13 @@
 
 import { BaseLanguageModel } from 'langchain/base_language';
 import { requestTimesFiltersChain } from '../../../../../server/langchain/chains/filter_generator';
+import { SearchRequest } from '../../../../../../../src/plugins/data/common';
 
-export async function addFilters(bodyQuery: object, userQuery: string, model: BaseLanguageModel) {
+export async function addFilters(
+  bodyQuery: SearchRequest['body'],
+  userQuery: string,
+  model: BaseLanguageModel
+) {
   const time = await requestTimesFiltersChain(model, userQuery);
   const timeFilter = {
     range: {
@@ -16,6 +21,6 @@ export async function addFilters(bodyQuery: object, userQuery: string, model: Ba
       },
     },
   };
-  // @ts-ignore
-  bodyQuery?.query.bool.must.push(timeFilter);
+  const must = bodyQuery?.query?.bool?.must;
+  if (Array.isArray(must)) must.push(timeFilter);
 }
